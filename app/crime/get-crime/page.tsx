efc805bfd0b9e3a379c667450f0891a58c3c7de5
giver of content <110511216+nathblade16@users.noreply.github.com>
2026-07-21 01:16:15 +0000

    start work on crime

---
+++ app/crime/get-crime/page.tsx
@@ -0,0 +1,135 @@
+export default function getCrimePage() {
+    return (
+        <div>
+            <div className="hero bg-base-200 min-h-screen">
+                <div className="hero-content text-center">
+                    <div className="max-w-md">
+                        <h1 className="text-5xl font-bold">Crime</h1>
+                        <p className="py-6">
+                            Crime makes every shop better by making shipping 0.1% faster and whatever other offers are on your tier!
+                        </p>
+                        <a href="#plans" className="btn btn-primary">See Plans</a>
+                    </div>
+                </div>
+            </div>
+            <div id="plans" className="flex flex-row items-center justify-center gap-4 py-6">
+                <div className="grid gap-6 md:grid-cols-3">
+                    <div className="card h-full w-96 bg-base-200 shadow-sm">
+                        <div className="card-body flex h-full flex-col">
+                            <span className="badge badge-xs badge-warning">Worst Option</span>
+                            <div className="flex justify-between">
+                                <h2 className="text-3xl font-bold">Crime</h2>
+                                <span className="text-xl">$99999999999999999/mo</span>
+                            </div>
+                            <ul className="mt-6 flex flex-col gap-2 text-xs">
+                                <li>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                                    <span>0.1% faster delivery</span>
+                                </li>
+                                <li className="opacity-50">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-base-content/50" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                                    <span className="line-through">Priority emotional support</span>
+                                </li>
+                                <li className="opacity-50">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-base-content/50" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                                    <span className="line-through">Crime Video</span>
+                                </li>
+                                <li className="opacity-50">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-base-content/50" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                                    <span className="line-through">Scamazon Alex Premium</span>
+                                </li>
+                                <li className="opacity-50">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-base-content/50" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                                    <span className="line-through">WaterOS Pro</span>
+                                </li>
+                                <li className="opacity-50">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-base-content/50" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                                    <span className="line-through">Free stickers in you order</span>
+                                </li>
+                            </ul>
+                            <div className="mt-auto">
+                                <button className="btn btn-primary btn-block">Subscribe</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card h-full w-96 bg-base-200 shadow-sm">
+                        <div className="card-body flex h-full flex-col">
+                            <span className="badge badge-xs badge-warning">Most Popular</span>
+                            <div className="flex justify-between">
+                                <h2 className="text-3xl font-bold">Crime Plus</h2>
+                                <span className="text-xl">$99999999999999999/day</span>
+                            </div>
+                            <ul className="mt-6 flex flex-col gap-2 text-xs">
+                                <li>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                                    <span>0.1% faster delivery</span>
+                                </li>
+                                <li>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                                    <span>Priority emotional support</span>
+                                </li>
+                                <li>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                                    <span>Crime Video</span>
+                                </li>
+                                <li>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                                    <span>Scamazon Alex Premium</span>
+                                </li>
+                                <li>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                                    <span>WaterOS Pro</span>
+                                </li>
+                                <li className="opacity-50">
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-base-content/50" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                                    <span className="line-through">Free stickers in you order</span>
+                                </li>
+                            </ul>
+                            <div className="mt-auto">
+                                <button className="btn btn-primary btn-block">Subscribe</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card h-full w-96 bg-base-200 shadow-sm">
+                        <div className="card-body flex h-full flex-col">
+                            <span className="badge badge-xs badge-warning">Recommended</span>
+                            <div className="flex justify-between">
+                                <h2 className="text-3xl font-bold">Crime Plus Plus</h2>
+                                <span className="text-xl">$99999999999999999/ms</span>
+                            </div>
+                            <ul className="mt-6 flex flex-col gap-2 text-xs">
+                                <li>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                                    <span>0.1% faster delivery</span>
+                                </li>
+                                <li>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                                    <span>Priority emotional support</span>
+                                </li>
+                                <li>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                                    <span>Crime Video</span>
+                                </li>
+                                <li>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                                    <span>Scamazon Alex Premium</span>
+                                </li>
+                                <li>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                                    <span>WaterOS Pro</span>
+                                </li>
+                                <li>
+                                    <svg xmlns="http://www.w3.org/2000/svg" className="size-4 me-2 inline-block text-success" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 13l4 4L19 7" /></svg>
+                                    <span>Free stickers in you order</span>
+                                </li>
+                            </ul>
+                            <div className="mt-auto">
+                                <button className="btn btn-primary btn-block">Subscribe</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
