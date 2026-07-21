7d37904df92611b4d6f9ebb2f85794ab8d401c3b
nathblade16 <nathblade16@proton.me>
2026-07-21 20:43:52 +1000

    Modify confirmation page 👍 and implement light mode 🤮

--- app/crime/confirmation/page.tsx
+++ app/crime/confirmation/page.tsx
@@ -1,13 +1,12 @@
+import Link from "next/link";
+
 export default function ConfirmationPage() {
     return (
-        <div className="flex flex-col flex-1 min-h-0 gap-4">
-            <div className="card bg-base-200 w-full shadow-sm">
-                <div className="card-body">
+        <div className="flex flex-col flex-1 min-h-0">
                     <h1 className="card-title text-2xl">Thank you for your purchase!</h1>
                     <br />
                     <p>You now have scamazon crime access!</p>
-                </div>
-            </div>
+                    <Link href="/" className="btn btn-primary mt-4 w-34">Take me home</Link>
         </div>
     )
 }
